4fefeb73c64c3f4a1c80477b6ac31b0cd4d5bdd4
Muhammad Hamza Qadir <66130102+hmzq-dev@users.noreply.github.com>
2025-04-24 14:44:56 +0500

    Output strings of variable lengths

--- fizzbuzz.asm
+++ fizzbuzz.asm
@@ -5,37 +5,44 @@
 ;   Check if divisible by 3 and prompt "fizz"
 ;   Check if divisible by 5 and prompt "buzz"
 ;   Check if divisible by 15 and prompt "fizzbuzz"
-; Modules needed: is_factor, output_prompt
-; Decide register convention for passing arguments
-
-; Memory conventions:
-; rax: immediate return value
-; r12: loop counter
-; r13: address of fizz, buzz, or fizzbuzz depending on what to output next
 
 section .data
-    fizz: db "fizz", 10, 13
-    .len: equ $ - fizz
-    buzz: db "buzz", 10, 13
-    .len: equ $ - buzz
-    fizzbuzz: db "fizzbuzz", 10, 13
-    .len: equ $ - fizzbuzz
+    fizz: db "fizz", 10, 13, 0
+    buzz: db "buzz", 10, 13, 0
+    fizzbuzz: db "fizzbuzz", 10, 13, 0
+
 
 section .text
     global _start
 
+
 _start:
-    mov r13, fizz
-    call output_result
+    mov rsi, fizz
+    call dynamic_output
     call exit
 
-output_result:
+
+dynamic_output:
+    ; Output a string with unknown length
+    ; mov string address to rsi before calling this function
     mov rax, 1
     mov rdi, 1
-    mov rsi, [r13]
-    mov rdx, [r13].len
+    ; Calculate string length
+    mov r11, 0
+    call calculate_length
+
+    mov rdx, r11
     syscall
     ret
+
+
+calculate_length:
+    mov r12b, [rsi + r11]
+    inc r11
+    cmp r12, 0
+    jne calculate_length
+    ret
+
 
 exit:
     mov rax, 60
