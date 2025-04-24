073167604535d83e78aec0ba489e3ece4d04de59
Muhammad Hamza Qadir <66130102+hmzq-dev@users.noreply.github.com>
2025-04-24 21:36:01 +0500

    Complete fizzbuzz

--- fizzbuzz.asm
+++ fizzbuzz.asm
@@ -1,15 +1,11 @@
 ; For every number between 1 and 100, output "fizz" if the number is divisible by 3,
 ; "buzz" if the number is divisible by 5, and "fizzbuzz" if it is divisible by both
-; Thinking
-; Loop 1 to 100
-;   Check if divisible by 3 and prompt "fizz"
-;   Check if divisible by 5 and prompt "buzz"
-;   Check if divisible by 15 and prompt "fizzbuzz"
 
 section .data
-    fizz: db "fizz", 10, 13, 0
-    buzz: db "buzz", 10, 13, 0
-    fizzbuzz: db "fizzbuzz", 10, 13, 0
+    fizz: db "fizz", 0
+    buzz: db "buzz", 0
+    plus: db 58, 32, 0
+    newline: db 10, 13, 0
 
 
 section .text
@@ -17,12 +13,64 @@ section .text
 
 
 _start:
-    mov rsi, fizz
-    call dynamic_output
+    ; r13 is loop counter
+    mov r13, 0
+    call iterate
     call exit
 
 
-dynamic_output:
+iterate:
+    inc r13
+
+    ; Print plus sign to signify new line
+    mov rsi, plus
+    call output
+
+    ; Check if divisible by 3
+    mov r14, 3
+    call get_remainder
+    call output_fizz
+
+    ; Check if divisible by 5
+    mov r14, 5
+    call get_remainder
+    call output_buzz
+
+    ; Print new line
+    mov rsi, newline
+    call output
+
+    cmp r13, 100
+    jne iterate
+    ret
+
+
+output_fizz:
+    cmp rdx, 0
+    jne return
+    mov rsi, fizz
+    call output
+    ret
+
+
+output_buzz:
+    cmp rdx, 0
+    jne return
+    mov rsi, buzz
+    call output
+    ret
+
+
+get_remainder:
+    ; Quotient in rax, divisor in r14
+    mov rax, r13
+    mov rdx, 0
+    div r14
+    ; Remainder is stored in rdx
+    ret
+
+
+output:
     ; Output a string with unknown length
     ; mov string address to rsi before calling this function
     mov rax, 1
@@ -49,3 +97,6 @@ exit:
     mov rdi, 0
     syscall
     
+
+return:
+    ret
